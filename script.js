846558aaddb5b937e951f8aff2258741d8355866
lingyingy9447 <91750525+lingyingy9447@users.noreply.github.com>
2021-10-01 12:53:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Ling Ying Yang"; // update the part BETWEEN the "quotes"
+let userName = "lingyingy9447"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
